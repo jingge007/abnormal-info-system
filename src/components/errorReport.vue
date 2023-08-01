4a4c11f7e7f249e0c459fe35418b249d0d761239
unknown <huangjing@tobosoft.com.cn>
2023-08-01 10:30:50 +0800

    优化json格式的数据

--- src/components/errorReport.vue
+++ src/components/errorReport.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="errorReport_box">
-    <h1 class="title_box">{{systemTitle + '异常错误报告'}}</h1>
-   <div class="content_box">
-     <div class="errorReport_item_box" v-for="item in errorReportList">
-       <h2 class="title">{{item.title}}</h2>
-       <div class="content_item_box">
-         <div class="options_box" v-if="optionskeyList.includes(item.paramsKey)">
-           <Icon title="一键复制内容" size="20" class="copyIcon" type="md-copy" @click="copyContentBtn(item.paramsValue)" />
-         </div>
-         <div class="content" :class="item.paramsKey === 'errorReportingInterface' ? 'setSizeStyle': ''">
-           {{item.paramsValue}}
-         </div>
-       </div>
-     </div>
-   </div>
+    <h1 class="title_box">{{ systemTitle + systemEnvironmentTitle + '异常错误报告' }}</h1>
+    <div class="content_box">
+      <div class="errorReport_item_box" v-for="item in errorReportList">
+        <h2 class="title">{{ item.title }}</h2>
+        <template v-if="optionskeyList.includes(item.paramsKey)">
+          <div class="content_item_box">
+            <div class="options_box">
+              <Icon title="一键复制内容" size="20" class="copyIcon" type="md-copy" @click="copyContentBtn(item.paramsKey)"/>
+            </div>
+            <div class="content" v-if="item.paramsValue">
+              <jsonView :data="item.paramsValue" :deep="5" theme="vs-code" :font-size="16" :line-height="25"></jsonView>
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <div class="content_item_box">
+            <div class="options_box" v-if="['errorReportingInterface'].includes(item.paramsKey)">
+              <Icon title="一键复制内容" size="20" class="copyIcon" type="md-copy" @click="copyContentBtn(item.paramsKey)"/>
+            </div>
+            <div class="content" :class="item.paramsKey === 'errorReportingInterface' ? 'setSizeStyle': ''">
+              {{ item.paramsValue }}
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import jsonView from 'vue-json-views'
 
 export default {
   name: "errorReport",
@@ -25,6 +38,8 @@ export default {
     let v = this;
     return {
       systemTitle: '',
+      systemEnvironmentTitle: '',
+      detailsInfo: {},
       errorReportList: [
         {title: '所在系统：', paramsKey: 'system', paramsValue: ''},
         {title: '系统环境：', paramsKey: 'systemEnvironment', paramsValue: ''},
@@ -32,49 +47,61 @@ export default {
         {title: '接口入參：', paramsKey: 'enteringGinseng', paramsValue: ''},
         {title: '报错信息：', paramsKey: 'errorMessage', paramsValue: ''},
       ],
-      optionskeyList: ['enteringGinseng', 'errorReportingInterface', 'errorMessage'],
-      systemObj:{
-        yms:'运营系统',
-        supplier:'供应商系统',
-        distributor:'分销商系统',
-        shopping:'商城',
-        dev:'dev环境',
-        test:'test环境',
-        staging:'staging环境'
+      optionskeyList: ['enteringGinseng', 'errorMessage'],
+      systemObj: {
+        yms: '运营系统',
+        supplier: '供应商系统',
+        distributor: '分销商系统',
+        shopping: '商城',
+        dev: '开发环境',
+        test: '测试环境',
+        staging: '生产环境'
       }
     }
   },
   created() {
-   this.getErrorData();
+    this.getErrorData();
   },
-  methods:{
+  methods: {
     // 一键复制内容
-    copyContentBtn(value) {
-      if (value) {
-        this.$tools.copyText(value)
-        this.$Message.success('复制成功！');
+    copyContentBtn(type) {
+      let val = '';
+      if (type === 'errorReportingInterface') {
+        val = this.detailsInfo[type];
+      } else {
+        val = this.detailsInfo[type];
       }
+      this.$tools.copyText(val)
+      this.$Message.success('复制成功！');
     },
     // 获取错误信息
     getErrorData() {
-      let v= this;
+      let v = this;
       let id = v.$route.query.id;
       if (id) {
         const query = new v.$leancloud.Query("abnormalList");
         query.get(id).then((data) => {
-          let obj =data.toJSON();
-          if (Object.keys(obj).length >0) {
+          let obj = data.toJSON();
+          v.detailsInfo = JSON.parse(JSON.stringify(obj));
+          if (Object.keys(obj).length > 0) {
             v.errorReportList.map((item) => {
-              for(let key in obj) {
+              for (let key in obj) {
                 if (item.paramsKey === key) {
                   if (['system', 'systemEnvironment'].includes(key)) {
                     let val = obj[key]
-                    if(key === 'system') {
+                    if (key === 'system') {
                       v.systemTitle = v.systemObj[val];
+                    }
+                    if (key === 'systemEnvironment') {
+                      v.systemEnvironmentTitle = v.systemObj[val];
                     }
                     item.paramsValue = v.systemObj[val];
                   } else {
-                    item.paramsValue = obj[key];
+                    if (v.optionskeyList.includes(key)) {
+                      item.paramsValue = JSON.parse(obj[key]);
+                    } else {
+                      item.paramsValue = obj[key];
+                    }
                   }
                 }
               }
@@ -84,6 +111,9 @@ export default {
         });
       }
     }
+  },
+  components: {
+    jsonView
   }
 }
 </script>
@@ -93,6 +123,7 @@ export default {
   width: 100%;
   height: 100vh;
   background-color: #F7F7F8;
+
   .title_box {
     width: 100%;
     font-size: 24px;
@@ -101,11 +132,13 @@ export default {
     padding: 20px 0;
     text-align: center;
   }
+
   .content_box {
     width: 1500px;
     padding: 20px;
     margin: 0 auto;
     box-sizing: border-box;
+
     .errorReport_item_box {
       width: 100%;
       display: flex;
@@ -115,7 +148,7 @@ export default {
       .title {
         text-align: right;
         width: 100px;
-        font-size: 15px;
+        font-size: 17px;
         color: #000;
         font-weight: bold;
       }
@@ -125,6 +158,7 @@ export default {
         overflow: hidden;
         border-radius: 5px;
         overflow: hidden;
+
         .options_box {
           width: 100%;
           height: 32px;
@@ -134,22 +168,29 @@ export default {
           display: flex;
           justify-content: flex-end;
           align-items: center;
+
           .copyIcon {
             font-size: 20px;
             cursor: pointer;
             color: #fff;
           }
         }
+
         .content {
           width: 100%;
-          background-color: #000000;
+          background-color: #1e1e1e;
           color: #fff;
           padding: 15px;
           font-size: 15px;
           font-weight: normal;
           word-wrap: break-word;
           word-break: break-all;
+
+          ///deep/ .json-view-container {
+          //  background-color: transparent;
+          //}
         }
+
         .setSizeStyle {
           font-size: 17px;
         }
